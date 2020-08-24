cddc6e63edb1376b8801d56be239b506bab40b0c
klawrence22 <ken.lawrence@gmail.com>
2020-08-24 10:48:08 -0600

    try to fix sql error in foriegn key constraint

--- models/item.py
+++ models/item.py
@@ -7,8 +7,10 @@ class ItemModel(db.Model):
     name = db.Column(db.String(80))
     price = db.Column(db.Float(precision=2))
 
-    store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
+#    store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
+    store_id = db.Column(db.Integer)
     store = db.relationship('StoreModel')
+
 
     def __init__(self, name, price, store_id):
         self.name = name
